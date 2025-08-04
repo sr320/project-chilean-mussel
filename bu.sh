b41fc9963e1d1698f0d2050c67cabb914deb2083
Steven Roberts <sr320@n3475.hyak.local>
2025-08-04 10:19:21 -0700

    bu

---
+++ bu.sh
@@ -0,0 +1,8 @@
+rsync -avz ../project-chilean-mussel \
+--exclude='*.sam' \
+--exclude='tmp*' \
+--exclude='*gz_C_to_T.fastq' \
+--exclude='*gz_G_to_A.fastq' \
+--exclude='Non_CpG_context*' \
+--exclude='.*' --exclude='*/.*' \
+sr320@gannet.fish.washington.edu:/volume1/v1_web/owlshell/bu-github/
